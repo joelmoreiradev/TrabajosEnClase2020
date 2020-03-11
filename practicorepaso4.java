f1016b4e7b4e3ca81cadbbd4ceb0a3b5864e7df8
Joel Moreira <51009134+joelmoreiradev@users.noreply.github.com>
2020-03-11 17:39:59 -0300

    Create practicorepaso4.java
    Falta continuarlo.

---
+++ practicorepaso4.java
@@ -0,0 +1,34 @@
+package javaapplication1;
+import java.util.Scanner;
+public class JavaApplication1 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+       
+        Scanner teclado = new Scanner(System.in);
+        
+         //creo un array
+        int num = 1; 
+        
+        int [] edad;
+        
+        while(num != 0){
+        System.out.println("Ingrese una edad");
+        num = teclado.nextInt();
+        edad = new int [num]; //asigno el valor de la variable num al array edad.
+        
+        
+        
+        
+        
+        
+      }
+        
+        
+        
+        
+        
+        
+    }
+    
+}
