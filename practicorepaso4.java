4b10ec66ee929c24979df604d1c027f583606c79
Joel Moreira <51009134+joelmoreiradev@users.noreply.github.com>
2020-03-19 18:51:10 -0300

    Update practicorepaso4.java

--- practicorepaso4.java
+++ practicorepaso4.java
@@ -1,34 +1,44 @@
-package javaapplication1;
 import java.util.Scanner;
-public class JavaApplication1 {
 
-    public static void main(String[] args) {
-        // TODO code application logic here
-       
+public class practico1 {
+    public static void main (String[]args) {
+
+
         Scanner teclado = new Scanner(System.in);
+
+        int num = 1;
+        int suma = 0;
+        int ingresados = 0;
+        int promedio;
+        int ingresadoMayor = 0;
+        int porcentajeMayores = 0;
         
-         //creo un array
-        int num = 1; 
-        
-        int [] edad;
-        
-        while(num != 0){
-        System.out.println("Ingrese una edad");
-        num = teclado.nextInt();
-        edad = new int [num]; //asigno el valor de la variable num al array edad.
-        
-        
-        
-        
-        
-        
-      }
-        
-        
-        
-        
-        
-        
+        while(num != 0) {
+
+            System.out.println("Ingrese una edad: ");
+            num = teclado.nextInt();
+            suma = suma + num;
+
+            //si num no es igual a 0, aumentar un numero ingresado.
+            //uso el if para que 0 no cuente como un ingreso y el promedio sea correcto.
+            if(num != 0){
+                ingresados++; //aumentar la cantidad de edades ingresadas
+            }
+
+            if(num >= 50){
+                ingresadoMayor++;
+            }
+
+        }
+
+        promedio = suma / ingresados;
+
+        porcentajeMayores =  (ingresadoMayor)*(100) / ingresados;
+        System.out.println("El promedio de las edades es " +promedio);
+        System.out.println("Se ingresaron "+ingresadoMayor+ " edades mayores a 50.");
+        System.out.println("El porcentaje de personas mayores de 50 años es "+porcentajeMayores+ "%");
+
+
+
     }
-    
 }
