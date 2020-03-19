b75a8eaf79895dca80264e41b19ebdac6d9d2c8a
Joel Moreira <51009134+joelmoreiradev@users.noreply.github.com>
2020-03-19 19:25:13 -0300

    Create practicorepaso5.java

---
+++ practicorepaso5.java
@@ -0,0 +1,11 @@
+public class practico1 {
+    public static void main (String[]args) {
+
+         for (int i = 1; i<= 100; i++){
+             System.out.println("3 * "+i + " = "+ 3*i);
+
+         }
+
+
+    }
+}
