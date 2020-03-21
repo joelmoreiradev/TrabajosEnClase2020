b076bbf9ce478a3c2cfc81b4b5652ed8d465664e
Joel Moreira <51009134+joelmoreiradev@users.noreply.github.com>
2020-03-20 22:41:21 -0300

    Create practicorepaso9.java

---
+++ practicorepaso9.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class practico1 {
+    public static void main (String[]args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        int [] miArreglo=new int [6];
+        int contador = 0;
+
+        for(int i = 0; i< 5; i++){
+            System.out.println("Ingrese un número: ");
+            miArreglo[i] = entrada.nextInt();
+
+        }
+
+        for(int i=0; i< 100 ; i++){
+            if(miArreglo[0] ==100* i +15){
+                contador++;
+            }
+        }
+
+        for(int i=0; i< 100 ; i++){
+            if(miArreglo[1] ==100* i +15){
+                contador++;
+            }
+        }
+
+        for(int i=0; i< 100 ; i++){
+            if(miArreglo[2] ==100* i +15){
+                contador++;
+            }
+        }
+
+        for(int i=0; i< 100 ; i++){
+            if(miArreglo[3] ==100* i +15){
+                contador++;
+            }
+        }
+
+        for(int i=0; i< 10000000 ; i++){
+            if(miArreglo[4] ==100* i +15){
+                contador++;
+            }
+        }
+
+
+        System.out.println(contador+" números terminan en 15");
+
+    }
+}
